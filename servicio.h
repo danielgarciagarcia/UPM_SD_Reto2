a79678ef1b6ec6a0126143e902bd69ac1e27c9ea
Daniel García <dgarciagarcia@protonmail.com>
2021-05-10 23:44:07 +0200

    Improved comments and format

--- servicio.h
+++ servicio.h
@@ -15,31 +15,31 @@ extern "C" {
 
 
 struct get_guid_reply {
-	int guid;
-	struct {
-		u_int uids_len;
-		int *uids_val;
-	} uids;
+    int guid;
+    struct {
+        u_int uids_len;
+        int *uids_val;
+    } uids;
 };
 typedef struct get_guid_reply get_guid_reply;
 
 struct gname_linked_list {
-	char *gname;
-	struct gname_linked_list *next;
+    char *gname;
+    struct gname_linked_list *next;
 };
 typedef struct gname_linked_list gname_linked_list;
 
 struct get_uname_reply_true {
-	char *uname;
-	struct gname_linked_list *linked_list;
+    char *uname;
+    struct gname_linked_list *linked_list;
 };
 typedef struct get_uname_reply_true get_uname_reply_true;
 
 struct get_uname_reply {
-	bool_t exists;
-	union {
-		get_uname_reply_true *reply;
-	} get_uname_reply_u;
+    bool_t exists;
+    union {
+        get_uname_reply_true *reply;
+    } get_uname_reply_u;
 };
 typedef struct get_uname_reply get_uname_reply;
 
@@ -48,12 +48,18 @@ typedef struct get_uname_reply get_uname_reply;
 
 #if defined(__STDC__) || defined(__cplusplus)
 #define GET_GUID 1
-extern  get_guid_reply * get_guid_1(char **, CLIENT *);
-extern  get_guid_reply * get_guid_1_svc(char **, struct svc_req *);
+
+extern get_guid_reply *get_guid_1(char **, CLIENT *);
+
+extern get_guid_reply *get_guid_1_svc(char **, struct svc_req *);
+
 #define GET_UNAME 2
-extern  get_uname_reply * get_uname_1(int *, CLIENT *);
-extern  get_uname_reply * get_uname_1_svc(int *, struct svc_req *);
-extern int reto_servicio_1_freeresult (SVCXPRT *, xdrproc_t, caddr_t);
+
+extern get_uname_reply *get_uname_1(int *, CLIENT *);
+
+extern get_uname_reply *get_uname_1_svc(int *, struct svc_req *);
+
+extern int reto_servicio_1_freeresult(SVCXPRT *, xdrproc_t, caddr_t);
 
 #else /* K&R C */
 #define GET_GUID 1
@@ -68,10 +74,14 @@ extern int reto_servicio_1_freeresult ();
 /* the xdr functions */
 
 #if defined(__STDC__) || defined(__cplusplus)
-extern  bool_t xdr_get_guid_reply (XDR *, get_guid_reply*);
-extern  bool_t xdr_gname_linked_list (XDR *, gname_linked_list*);
-extern  bool_t xdr_get_uname_reply_true (XDR *, get_uname_reply_true*);
-extern  bool_t xdr_get_uname_reply (XDR *, get_uname_reply*);
+
+extern bool_t xdr_get_guid_reply(XDR *, get_guid_reply *);
+
+extern bool_t xdr_gname_linked_list(XDR *, gname_linked_list *);
+
+extern bool_t xdr_get_uname_reply_true(XDR *, get_uname_reply_true *);
+
+extern bool_t xdr_get_uname_reply(XDR *, get_uname_reply *);
 
 #else /* K&R C */
 extern bool_t xdr_get_guid_reply ();
